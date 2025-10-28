53b40d0d0abbf6cb1d59c195eb185b024439a734
Khushi Johri <khushijohri2001@gmail.com>
2025-10-28 13:29:57 +0530

    Cuboid with Mouse Movements

--- src/main.js
+++ src/main.js
@@ -191,6 +191,72 @@
 // ----------------------------------------------------------------------------------
 // #6 Earth Animation with Textures
 
+// import * as THREE from "three";
+// import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+
+// const sphereGeometry = new THREE.SphereGeometry(1, 70, 70);
+// const sphereMaterial = new THREE.MeshBasicMaterial();
+// const earthStructure = new THREE.Mesh(sphereGeometry, sphereMaterial);
+// scene.add( earthStructure );
+
+// const sphereGeometry2 = new THREE.SphereGeometry(1.015, 70, 70);
+// const sphereMaterial2 = new THREE.MeshBasicMaterial();
+// const earthCloudsStructure = new THREE.Mesh(sphereGeometry2, sphereMaterial2);
+// scene.add( earthCloudsStructure );
+
+
+// //Textures
+// let textureLoader = new THREE.TextureLoader();
+
+// let earthTexture = textureLoader.load("../public/textures/earth_daymap.jpg");
+// earthTexture.colorSpace = THREE.SRGBColorSpace;
+// sphereMaterial.map = earthTexture;
+
+// let earthCloudsTexture = textureLoader.load("../public/textures/earth_clouds_map.jpg");
+// earthCloudsTexture.colorSpace = THREE.SRGBColorSpace;
+// sphereMaterial2.map = earthCloudsTexture;
+// sphereMaterial2.transparent = true;
+// sphereMaterial2.opacity = 0.4;
+
+// camera.position.z = 2;
+
+// const canvas = document.querySelector(".world");
+// const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.dampingFactor = 0.05;
+
+// const clock = new THREE.Clock();
+
+// function animate() {
+//   window.requestAnimationFrame(animate);
+//   const elapsedTime = clock.getElapsedTime();
+
+//   controls.update();
+
+//   earthStructure.rotation.y = elapsedTime * 0.1;
+//   earthCloudsStructure.rotation.y = elapsedTime * 0.15;
+
+//   renderer.render(scene, camera);
+// }
+// renderer.setAnimationLoop(animate);
+
+
+
+// ----------------------------------------------------------------------------------
+// #7 Cuboid with Mouse Movements
+
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
@@ -202,56 +268,38 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+const cuboidGeometry = new THREE.BoxGeometry(1, 3, 6);
+const cuboidMaterial = new THREE.MeshBasicMaterial({
+  color: "green",
+  wireframe: true,
+});
+const cuboid = new THREE.Mesh(cuboidGeometry, cuboidMaterial);
+scene.add(cuboid);
 
-const sphereGeometry = new THREE.SphereGeometry(1, 70, 70);
-const sphereMaterial = new THREE.MeshBasicMaterial();
-const earthStructure = new THREE.Mesh(sphereGeometry, sphereMaterial);
-scene.add( earthStructure );
+const mouse = {
+  x:0,
+  y:0
+}
 
-const sphereGeometry2 = new THREE.SphereGeometry(1.015, 70, 70);
-const sphereMaterial2 = new THREE.MeshBasicMaterial();
-const earthCloudsStructure = new THREE.Mesh(sphereGeometry2, sphereMaterial2);
-scene.add( earthCloudsStructure );
+window.addEventListener("mousemove", (e) => {
+  mouse.x = e.clientX/window.innerWidth;
+  mouse.y = e.clientY/innerHeight;
+})
 
-
-//Textures
-let textureLoader = new THREE.TextureLoader();
-
-let earthTexture = textureLoader.load("../public/textures/earth_daymap.jpg");
-earthTexture.colorSpace = THREE.SRGBColorSpace;
-sphereMaterial.map = earthTexture;
-
-let earthCloudsTexture = textureLoader.load("../public/textures/earth_clouds_map.jpg");
-earthCloudsTexture.colorSpace = THREE.SRGBColorSpace;
-sphereMaterial2.map = earthCloudsTexture;
-sphereMaterial2.transparent = true;
-sphereMaterial2.opacity = 0.4;
-
-camera.position.z = 2;
+camera.position.z = 8;
 
 const canvas = document.querySelector(".world");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
-
-const clock = new THREE.Clock();
-
 function animate() {
   window.requestAnimationFrame(animate);
-  const elapsedTime = clock.getElapsedTime();
 
-  controls.update();
-
-  earthStructure.rotation.y = elapsedTime * 0.1;
-  earthCloudsStructure.rotation.y = elapsedTime * 0.15;
+  cuboid.lookAt(new THREE.Vector3(mouse.x - 0.5, -mouse.y + 0.5, 1));
 
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
-
 
 
 
